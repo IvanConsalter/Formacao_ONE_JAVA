fdf63a35f49c63e41bd54ab0349c153a56fb8ddb
IvanConsalter <ivanconsalter1@gmail.com>
2020-04-19 11:20:51 -0300

    Set no modelo, programacao defensiva com conjuntos

--- 08_Java_parte08_collections/aulas/java-collections/src/br/com/collections/Curso.java
+++ 08_Java_parte08_collections/aulas/java-collections/src/br/com/collections/Curso.java
@@ -1,15 +1,18 @@
 package br.com.collections;
 
-import java.util.List;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 public class Curso {
-
+	
 	private String curso;
 	private String instrutor;
+	private List<Aula> aulas = new LinkedList<Aula>(); 
 	//private int tempoTotal;
-	private List<Aula> aulas = new LinkedList<Aula>();
+	private Set<Aluno> alunos = new HashSet<>();
 	
 	public Curso(String curso, String instrutor) {
 		this.curso = curso;
@@ -24,28 +27,35 @@ public class Curso {
 		return instrutor;
 	}
 	
-	public int getTempoTotal() {
-		//return tempoTotal;
-		//tempoTotal co stream
-		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
-	}
-	
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(aulas);
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
 	}
 	
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 		//this.tempoTotal += aula.getTempo();
 	}
+	
+	public int getTempoTotal() {
+		//return tempoTotal;
+		//metodo com strem
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
 
-	@Override
-	public String toString() {
-		return "[Curso: " + this.curso
-				+ ", tempo total: "
-				+ this.getTempoTotal() + "min"
-				+ ", aulas: "
-				+ this.getAulas() + "]";
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
 	}
 	
+	@Override
+	public String toString() {
+		return "[Curso: " + this.curso 
+				+ ", tempo total: " 
+				+ this.getTempoTotal() + "min" 
+				+ ", aulas: " 
+				+ this.getAulas() + "]";
+	}
 }
---
+++ 08_Java_parte08_collections/aulas/java-collections/src/br/com/collections/TestaCursoComAluno.java
@@ -0,0 +1,44 @@
+package br.com.collections;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class TestaCursoComAluno {
+	
+	public static void main(String[] args) {
+		
+		Curso java = new Curso("java-8", "Paulo Silveira");
+		
+		java.adiciona(new Aula("Tipos primitivos", 12));
+		java.adiciona(new Aula("Variaveis", 16));
+		java.adiciona(new Aula("Condicionais", 14));
+		
+		
+		java.matricula(new Aluno("Andre Silveira", 222333));
+		java.matricula(new Aluno("Roberto Siqueira", 333222));
+		java.matricula(new Aluno("Caio Berlanda", 123456));
+		
+		//iteracao com lambda
+		java.getAlunos().forEach(aluno -> {
+			System.out.println(aluno);
+		});
+		
+		/*
+		//erro UnsupportedOperationException
+		//Collections$UnmodifiableCollection
+		Set<Aluno> alunos = java.getAlunos();
+		
+		Aluno novoAluno = new Aluno("Carlos Roberto", 654321);
+		alunos.add(novoAluno);
+		System.out.println(novoAluno);
+		*/
+		
+		Set<Aluno> alunosSincronizados = Collections.synchronizedSet(java.getAlunos());
+		System.out.println(alunosSincronizados);
+		
+	}
+
+	
+	
+	
+}
